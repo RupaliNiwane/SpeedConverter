6488586a2388e29b67d80993377edca2d8ecafcb
unknown <rupaliniwane459@gmail.com>
2023-12-07 19:34:50 +0530

    speed

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Calu from './Components/Calu';
+
+const App = () => {
+  return (
+     <> 
+       <div className='container'>
+ 			 <h1 className='text-center mt-5'>Speed Converter !!!</h1>
+       	   <Calu/>
+          </div>  
+      </> 
+  );
+}
+
+export default App;
+
+
+
